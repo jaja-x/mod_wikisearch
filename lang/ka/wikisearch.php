73fb42b3f091da7d6ddf28c7071aef031a42ead2
jaja-x <93482600+jaja-x@users.noreply.github.com>
2021-11-01 15:50:48 +0400

    Create wikisearch.php

---
+++ lang/ka/wikisearch.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_wikisearch
+ * @category    string
+ * @copyright   2021 LEARNING.BOG.GE <info@learning.bog.ge>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Wiki ძიება';
+$string['modulenameplural'] = 'Wiki ძიება';
+$string['modulename_help'] = 'ეს მოდული საშუალებას გაძლევთ მოძებნოთ მითითებული ფრაზები Wiki–ის ყველა ინსტანციაში.';
+$string['modulename_link'] = 'Wiki_activity';
+
+$string['pluginname'] = 'Wiki ძიება';
+$string['wikisearch:addinstance'] = 'Wiki ძიება';
+$string['wikisearchname'] = 'Wiki ძიების დასახელება';
+$string['wikisearchname_help'] = 'მიუთითეთ Wiki ძიების დასახელება';
+$string['pluginadministration'] = 'Wiki ძიება';
+$string['nonewmodules'] = 'no new modules';
+$string['missingidandcmid'] = 'Missing id and cmid';
+
+$string['searchfield'] = 'ძიება Wiki-ში:';
+$string['searchfieldplaceholder'] = 'მოძებნეთ ინფორმაცია Wiki გვერდებზე ...';
+$string['search'] = 'ძიება';
+$string['viewwikipage'] = 'სრულად ნახვა';
+$string['searchresults'] = 'ძიების შედეგები: ';
